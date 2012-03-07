f39394f42a77f6fbb78c07d5bbd9f74bf8625a24
Bao Pham <gbaopham@gmail.com>
2012-03-07 12:32:09 -0800

    Rename folder scripts to js

--- static/js/syntaxmain.js
+++ static/js/syntaxmain.js
@@ -3,7 +3,7 @@ function path() {
         result = []
             ;
     for(var i = 0; i < args.length; i++)
-        result.push(args[i].replace('@', '/scripts/'));
+        result.push(args[i].replace('@', '/js/'));
     return result
 }
 SyntaxHighlighter.autoloader.apply(null, path(
